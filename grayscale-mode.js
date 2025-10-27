a173397cfd5bbd5e91e64c10b44c027eb2a2cb5e
AllaboutITAppScript <121791279+AllaboutITAppScript@users.noreply.github.com>
2025-10-27 11:37:46 +0700

    Update grayscale-mode.js

--- grayscale-mode.js
+++ grayscale-mode.js
@@ -1,6 +1,6 @@
- (function(){
+    (function(){
         var s=document.createElement('style');
         s.id='grayscale-cdn';
-        s.textContent='html{filter:grayscale(50%)!important;-webkit-filter:grayscale(50%)!important}';
+        s.textContent='html{filter:grayscale(100%)!important;-webkit-filter:grayscale(85%)!important}';
         document.head.appendChild(s);
     })();
